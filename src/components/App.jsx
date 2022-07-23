c35e6d92be5cedab379807795e211f48c8f120fe
jokerav <jokeravdeveloper@gmail.com>
2022-07-23 17:43:15 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import RegisterForm from './RegisterForm/RegisterForm';
 import { Routes, Route } from 'react-router-dom';
-import PrivateRoute from 'Routes/PrivateRoutes';
-import ProtectedRoute from 'Routes/ProtectedRoute';
+// import PrivateRoute from 'Routes/PrivateRoutes';
+// import ProtectedRoute from 'Routes/ProtectedRoute';
 import Header from './PageHeader/PageHeader';
 import LoginForm from './LoginForm/LoginForm';
 import Contacts from './Contacts/Contacts';
-import { useSelector } from 'react-redux';
-import { getLoggedIn } from '../contactsAPI/selectors';
+// import { useSelector } from 'react-redux';
+// import { getLoggedIn } from '../contactsAPI/selectors';
 
 export const App = () => {
-  const isLoggedin = useSelector(getLoggedIn);
+  // const isLoggedin = useSelector(getLoggedIn);
 
   return (
     <div>
