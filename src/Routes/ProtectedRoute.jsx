ff6dbf2838491c383b17eff2565f28b7f5d204a3
jokerav <jokeravdeveloper@gmail.com>
2022-07-23 19:02:09 +0300

    </ProtectedRoute>

--- src/Routes/ProtectedRoute.jsx
+++ src/Routes/ProtectedRoute.jsx
@@ -1,12 +1,7 @@
 import { Navigate } from 'react-router-dom';
-const ProtectedRoute = ({
-  isAllowed,
-  redirectPath = '/contacts',
-  children,
-}) => {
-  if (isAllowed) {
-    console.log('Protected Route! Navigate to contact!');
-    return <Navigate to={redirectPath} replace />;
+const ProtectedRoute = ({ user, children }) => {
+  if (user) {
+    return <Navigate to="/contacts" replace />;
   }
 
   return children;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,38 @@
 import RegisterForm from './RegisterForm/RegisterForm';
 import { Routes, Route } from 'react-router-dom';
 import PrivateRoute from 'Routes/PrivateRoutes';
-// import ProtectedRoute from 'Routes/ProtectedRoute';
+import ProtectedRoute from 'Routes/ProtectedRoute';
 import Header from './PageHeader/PageHeader';
 import LoginForm from './LoginForm/LoginForm';
 import Contacts from './Contacts/Contacts';
-// import { useSelector } from 'react-redux';
-// import { getLoggedIn } from '../contactsAPI/selectors';
+import { useSelector } from 'react-redux';
+import { getLoggedIn } from '../contactsAPI/selectors';
 
 export const App = () => {
-  // const isLoggedin = useSelector(getLoggedIn);
+  const isLoggedin = useSelector(getLoggedIn);
 
   return (
     <div>
       <Header />
       <Routes>
-        <Route exact path="/register" element={<RegisterForm />}></Route>
-        <Route exact path="/login" element={<LoginForm />}></Route>
-        {/* <Route path="/contacts" element={<Contacts />}></Route> */}
+        <Route
+          exact
+          path="/register"
+          element={
+            <ProtectedRoute user={isLoggedin}>
+              <RegisterForm />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route
+          exact
+          path="/login"
+          element={
+            <ProtectedRoute user={isLoggedin}>
+              <LoginForm />
+            </ProtectedRoute>
+          }
+        ></Route>
         <Route
           path="/contacts"
           element={
