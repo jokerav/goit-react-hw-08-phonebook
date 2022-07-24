fb9c83da7db72a2495905de7b10e7f32f7789f7a
jokerav <jokeravdeveloper@gmail.com>
2022-07-24 14:29:42 +0300

    Update ErrorPage.jsx

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,22 @@
-import { Empty } from 'antd';
+import { Button, Empty } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const ErrorPage = () => <Empty />;
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <Empty
+      image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+      imageStyle={{
+        height: 60,
+      }}
+      description={<span>Page not found</span>}
+    >
+      <Button type="primary" onClick={() => navigate('/login')}>
+        Back
+      </Button>
+    </Empty>
+  );
+};
 
 export default ErrorPage;
